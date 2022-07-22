1a772f086d51668de55ad683755a75fbf6889498
sungwook.choi <sungwook.csw@gmail.com>
2022-07-22 20:39:18 +0900

    feat: make a simple fastapi health check app

---
+++ simple-fastapi-app/main.py
@@ -0,0 +1,28 @@
+"""
+main.py for run fastapi
+"""
+from datetime import timezone, timedelta, datetime
+
+from fastapi import FastAPI
+
+import uvicorn
+
+KST = timezone(timedelta(hours=9))
+PRETTY_TIME_FORMAT = "%y년%m월%d일, %H시%M분%S초"
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    """
+    Health check api for index
+
+    return string format server time
+    """
+    now: datetime = datetime.now(tz=KST)
+    str_time: str = now.strftime(PRETTY_TIME_FORMAT)
+    return {"message": str_time}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
